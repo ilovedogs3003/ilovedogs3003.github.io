b6fb20cbb6feea967d3789eb51c703a91a83a585
ilovedogs3003 <jal2348@columbia.edu>
2025-05-29 15:08:43 +0000

    Deploying to gh-pages from @ ilovedogs3003/ilovedogs3003.github.io@75ad2d1c8d7818a19d6d4f5a51fa2305780208a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of different projects, both personal and professional.",
+          description: "A growing collection of different personal, academic, and personal projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
